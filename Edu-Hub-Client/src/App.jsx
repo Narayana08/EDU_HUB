08ea07f936aa91a222f39f6b13eaeb66546ebc30
Narayana08 <narayana.c@techouts.com>
2024-10-13 09:44:04 +0530

    Initial commit

---
+++ Edu-Hub-Client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h6>Welcome, to the EduHub Platform</h6>
+      <Button className="bg-red-200">Click here</Button>
+    </>
+  )
+}
+
+export default App
